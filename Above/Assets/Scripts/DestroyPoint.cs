4e6f38742b7d394c05e45c49007c743cc100d0fd
NeuronStud1o <122213370+NeuronStud1o@users.noreply.github.com>
2023-02-24 16:00:45 +0200

    Above
    Part Vakulenko Maryan

---
+++ Above/Assets/Scripts/DestroyPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPoint : MonoBehaviour
+{
+    public GameObject destroyPoint;
+
+    void Start()
+    {
+        destroyPoint = GameObject.Find("DestroyPoint");
+    }
+    void Update()
+    {
+        if (transform.position.y < destroyPoint.transform.position.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
